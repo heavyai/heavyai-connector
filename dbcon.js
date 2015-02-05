d78ccac1c91e7c346aaed6d45062a5a5b97502df
Todd Mostak <tmostak@gmail.com>
2015-02-05 00:40:31 -0800

    Added dbcon.js - ready to also test crossfilter.mapd.js

---
+++ dbcon.js
@@ -0,0 +1,52 @@
+(function(exports){
+  dbcon.version = "1.0";
+  function dbcon() {
+    var dbcon = {
+      setPlatform: setPlatform
+      setHost: setHost
+      setUser: setUser,
+      setDbName: setDbName,
+      query: query,
+    }
+    var platform = "postgres";
+    var host = "localhost"; // default
+    var user = null;
+    var dbName = null;
+
+    function setPlatform(newPlatform) {
+      platform = newPlatofrm;
+      return dbcon;
+    }
+
+    function setHost (newHost) {
+      host = newHost;
+      return dbcon;
+    }
+
+    function setUser (newUser) {
+      user = newUser;
+      return dbcon;
+    }
+
+    function setDbName (newDb) {
+      dbName = newDb;
+      return dbcon;
+    }
+
+    function query(query) {
+      if (host == null) {
+        throw "Host not defined";
+      }
+      if (user == null) {
+        throw "User not defined";
+      }
+      if (dbName == null) {
+        throw "Db name not defined";
+      }
+
+      var requestString = platform + ".php?hos=t" + host + "&user=" + user + "&dbname=" + dbName + "&q" +  query;
+      return JSON.parse($.ajax({type: "GET", url: requestString, async: false}).responseText);
+    }
+    return dbcon;
+  }
+})(typeof exports !== 'undefined' && exports || this);
