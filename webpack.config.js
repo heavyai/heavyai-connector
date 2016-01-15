bb8abbdf65116bf7ec2048268f13df635c688298
Danny Delott <dannydelott@gmail.com>
2016-01-14 17:58:50 -0800

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-    entry: "./src/mapd-con-es6.js",
+    entry: [
+      "./src/mapd-con-es6.js"
+    ],
     output: {
         path: __dirname,
         filename: "mapd-con.js"
     },
-    module: {
+    module: { 
       loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        {test: /\.js$/, loader: "babel-loader", exclude: /node_modules/}
       ]
-    }
+    },
+    ecmaFeatures: { modules: true }
 };
