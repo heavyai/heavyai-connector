8ea003191f6b0956e55ec9df83b5a60753c6fea6
Danny Delott <dannydelott@gmail.com>
2016-02-02 12:27:57 -0800

    Update npm scripts and webpack config
    - Use npm start to build the entire project, run tests, and generate
      docs
    - Move linting out of webpack and into npm lint script

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
         filename: 'MapdCon.js'
     },
     module: { 
-      preLoaders: [
-        {test: /\mapd-con-es6.js$/, loaders: ["eslint-loader"], exclude: /node_modules/}
-      ],
       loaders: [
         {test: /\.js$/, loader: "babel-loader", exclude: /node_modules/}
       ]
