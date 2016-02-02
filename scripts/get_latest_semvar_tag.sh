5f90cd7ecc390e764dcaf9e88d43d2f7e9c5e111
Danny Delott <dannydelott@gmail.com>
2016-02-02 13:03:14 -0800

    Refactor get_latest_semvar_tag.sh
    - Look at the entire commit message (subject and body) for the semvar
      label

--- scripts/get_latest_semvar_tag.sh
+++ scripts/get_latest_semvar_tag.sh
@@ -4,27 +4,27 @@
 i=0;
 while read commit; do
 
-        i=$(($i+1))
+    i=$(($i+1))
 
-        # Parse the commit
-        HASH=`echo $commit | cut -d '|' -f1`
-        MESSAGE=`echo $commit | cut -d '|' -f2`
+    # Parse the commit
+    HASH=`echo $commit | cut -d '|' -f1`
+    MESSAGE=`echo $commit | cut -d '|' -f2`
 
-        # Print out the commit
-        echo $i. $'\t' $HASH $MESSAGE
+    # Print out the commit
+    echo $i. $'\t' $HASH $MESSAGE
 
-        # Check the commit for semvar tag
-        SEM_VAR=$(echo $MESSAGE \
-                | grep -o '\[\(patch\|major\|minor\)\]')
+    # Check the commit for semvar tag
+    SEM_VAR=$(echo $MESSAGE \
+            | grep -o '\[\(patch\|major\|minor\)\]')
 
-        # Set semvar tag to variable
-        if [ "$SEM_VAR" ]; then
-                SEM_VAR_TAG=$(echo $SEM_VAR \
-                        | grep -o '\(patch\|major\|minor\)')
-                break
-        fi
+    # Set semvar tag to variable
+    if [ "$SEM_VAR" ]; then
+            SEM_VAR_TAG=$(echo $SEM_VAR \
+                    | grep -o '\(patch\|major\|minor\)')
+            break
+    fi
 
-done < <( git log --pretty="%H|%s" --skip=1 )
+done < <( git log --pretty="%H|%B" --skip=1 )
 
 #print the variable
 echo $SEM_VAR_TAG
