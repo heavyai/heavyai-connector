8628b0111731a6f61095d96076ce082acde06716
Danny Delott <dannydelott@gmail.com>
2016-02-05 11:13:28 -0800

    Add test for #createFrontendView

---
+++ test/createFrontendView.spec.js
@@ -0,0 +1,26 @@
+// NPM Dependencies
+import expect from 'expect';
+import { jsdom } from 'jsdom';
+
+// Custom Dependences
+import { connect, loadScripts, randomString } from './utils';
+import { html } from './mocks';
+
+// JSDom Configuration
+const scripts = loadScripts();
+
+describe('MapdCon#createFrontendView', () => {
+  it('should create a new frontend view with name "test_view"', (done) => {
+    const test = (err, window) => {
+      const viewState = randomString(10);
+      const imageHash = randomString(8, 'n');
+      const mapdcon = connect(new window.MapdCon())
+        .createFrontendView('test_view', viewState, imageHash);
+      const view = mapdcon.getFrontendView('test_view');
+      expect(view.view_state).toEqual(viewState);
+      expect(view.image_hash).toEqual(imageHash);
+      done();
+    };
+    jsdom.env({ html, src: scripts, done: test });
+  });
+});
--- test/utils/index.js
+++ test/utils/index.js
@@ -18,3 +18,27 @@ export const loadScripts = () => {
   const mapdcon = readFileSync('./dist/MapdCon.js', 'utf-8');
   return [thrift, mapdthrift, mapdtypes, mapdcon];
 };
+
+/**
+ * Return a random alpha-numeric string.
+ * @param {Number} len - length of the string to create
+ * @param {String} [an] - 'A' for alpha only, 'N' for numeric only
+ * @return {String} randomString
+ * @example
+ * randomString(10);        // "4Z8iNQag9v"
+ * randomString(10, "A");   // "aUkZuHNcWw"
+ * randomString(10, "N");   // "9055739230"
+ */
+export const randomString = (len, an) => {
+  an = an && an.toLowerCase();
+  let str = '';
+  const min = an === 'a' ? 10 : 0;
+  const max = an === 'n' ? 10 : 62;
+  for (let i = 0; i < len; i++) {
+    let r = Math.random() * (max - min) + min << 0;
+    const s = r < 36 ? 55 : 61;
+    const t = r > 9 ? s : 48;
+    str += String.fromCharCode(r += t);
+  }
+  return str;
+};
