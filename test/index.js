8f9dedd7453129b5862caf2065abdcb742bf8c35
Danny Delott <dannydelott@gmail.com>
2016-02-08 10:22:19 -0800

    Specify the order in which tests will run

---
+++ test/index.js
@@ -0,0 +1,10 @@
+// Run tests in specific order
+require('./basic-methods.spec.js');
+require('./getters-setters.spec.js');
+require('./connection.spec.js');
+require('./link.spec.js');
+require('./createFrontendView.spec.js');
+require('./getFrontendViews.spec.js');
+require('./getFrontendView.spec.js');
+require('./detectColumnTypes.spec.js');
+require('./deleteFrontendView.spec.js'); // Must be last!
