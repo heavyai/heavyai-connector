7cb5c634010eb87cf78fba73bdcd62864f66ceae
Danny Delott <dannydelott@gmail.com>
2016-04-11 17:31:18 -0700

    Add build_output.txt to libaries build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,14 @@
-npm run babel && npm run webpack
+npm run babel | tee build_output.txt
+grep -q -i 'error' build_output.txt
+if [ "$?" == "0" ]; then
+  exit 1
+fi
+
+npm run webpack | tee build_output.txt
+grep -q -i 'error' build_output.txt
+if [ "$?" == "0" ]; then
+  exit 1
+fi
+
+rm build_output.txt
+
