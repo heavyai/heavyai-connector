ff694b5eaa192f5de7ffd82eae14f5944746732d
Danny Delott <dannydelott@gmail.com>
2016-04-20 11:22:19 -0700

    Fix test config

--- test/config.js
+++ test/config.js
@@ -1,10 +1,11 @@
 // TEST SERVER CONNECTION PARAMETERS
-process.env.PROTOCOL = 'https';
+process.env.PROTOCOL = 'http';
 process.env.HOST = 'kali.mapd.com';
 process.env.PORT = 9092;
 process.env.DB_NAME = 'mapd';
 process.env.USER = 'mapd';
-process.env.PASSWORD = 'mapd';
+process.env.PASSWORD = 'HyperInteractive';
+process.env.TABLE_NAME = 'contributions';
 
 // DO NOT EDIT BELOW THIS LINE
 process.env.NODE_ENV = 'test';
