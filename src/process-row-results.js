0484de6f294277092a9276c9948fff1843875f55
Jonathan Huang <grokut@gmail.com>
2016-06-28 12:56:55 -0700

    [V2 CHARTS] refactor chart updates to account for async data and coloring, fixes #956
    [TABLE] fix table chart creation - do not apply color

---
+++ src/process-row-results.js
@@ -0,0 +1,122 @@
+/**
+   * It should be avoided to query for row-based results from the server, howerver
+   * it can still be done. In this case, still process them into the same format as
+   * (@link processColumnarResults} to keep the output consistent.
+   * @param {TRowSet} data - The row-based data returned from a query
+   * @param {Boolean} eliminateNullRows
+   * @returns {Object} processedResults
+   */
+export default function processRowResults(data, eliminateNullRows, datumEnum) {
+  console.log(data, eliminateNullRows, datumEnum)
+  const numCols = data.row_desc.length;
+  const formattedResult = { fields: [], results: [] };
+
+  formattedResult.fields = data.row_desc.map((field) => {
+    return {
+      name: field.col_name,
+      type: datumEnum[field.col_type.type],
+      is_array: field.col_type.is_array,
+    };
+  });
+
+  formattedResult.results = [];
+  let numRows = 0;
+  if (data.rows !== undefined && data.rows !== null) {
+    numRows = data.rows.length; // so won't throw if data.rows is missing
+  }
+
+  for (let r = 0; r < numRows; r++) {
+    if (eliminateNullRows) {
+      let rowHasNull = false;
+      for (let c = 0; c < numCols; c++) {
+        if (data.rows[r].columns[c].is_null) {
+          rowHasNull = true;
+          break;
+        }
+      }
+      if (rowHasNull) {
+        continue;
+      }
+    }
+
+    const row = {};
+    for (let c = 0; c < numCols; c++) {
+      const fieldName = formattedResult.fields[c].name;
+      const fieldType = formattedResult.fields[c].type;
+      const fieldIsArray = formattedResult.fields[c].is_array;
+      if (fieldIsArray) {
+        if (data.rows[r].cols[c].is_null) {
+          row[fieldName] = 'NULL';
+          continue;
+        }
+        row[fieldName] = [];
+        const arrayNumElems = data.rows[r].cols[c].val.arr_val.length;
+        for (let e = 0; e < arrayNumElems; e++) {
+          const elemDatum = data.rows[r].cols[c].val.arr_val[e];
+          if (elemDatum.is_null) {
+            row[fieldName].push('NULL');
+            continue;
+          }
+          switch (fieldType) {
+            case 'BOOL':
+              row[fieldName].push(elemDatum.val.int_val ? true : false);
+              break;
+            case 'SMALLINT':
+            case 'INT':
+            case 'BIGINT':
+              row[fieldName].push(elemDatum.val.int_val);
+              break;
+            case 'FLOAT':
+            case 'DOUBLE':
+            case 'DECIMAL':
+              row[fieldName].push(elemDatum.val.real_val);
+              break;
+            case 'STR':
+              row[fieldName].push(elemDatum.val.str_val);
+              break;
+            case 'TIME':
+            case 'TIMESTAMP':
+            case 'DATE':
+              row[fieldName].push(elemDatum.val.int_val * 1000);
+              break;
+            default:
+              break;
+          }
+        }
+      } else {
+        const scalarDatum = data.rows[r].cols[c];
+        if (scalarDatum.is_null) {
+          row[fieldName] = 'NULL';
+          continue;
+        }
+        switch (fieldType) {
+          case 'BOOL':
+            row[fieldName] = scalarDatum.val.int_val ? true : false;
+            break;
+          case 'SMALLINT':
+          case 'INT':
+          case 'BIGINT':
+            row[fieldName] = scalarDatum.val.int_val;
+            break;
+          case 'FLOAT':
+          case 'DOUBLE':
+          case 'DECIMAL':
+            row[fieldName] = scalarDatum.val.real_val;
+            break;
+          case 'STR':
+            row[fieldName] = scalarDatum.val.str_val;
+            break;
+          case 'TIME':
+          case 'TIMESTAMP':
+          case 'DATE':
+            row[fieldName] = new Date(scalarDatum.val.int_val * 1000);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    formattedResult.results.push(row);
+  }
+  return formattedResult;
+}
