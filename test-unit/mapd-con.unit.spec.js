e0ee2134be6d47212feda0d8b387ad4bd79452b0
Jonathan Huang <grokut@gmail.com>
2016-07-07 21:52:21 -0700

    [MAPDCON] begin testing mapd-con and setup istanbul

---
+++ test-unit/mapd-con.unit.spec.js
@@ -0,0 +1,31 @@
+
+import chai, {expect} from "chai"
+
+
+describe("MapdCon", () => {
+  let mapdcon
+  before(() => {
+    mapdcon = require("../src/mapd-con-es6").default
+    mapdcon._sessionId = "SESSION_ID"
+  })
+  function stubClient (client) {
+    mapdcon._client = [client]
+  }
+  describe("getFrontendViewsAsync", () => {
+    const views = [{}, {}, {}]
+    before(() => {
+      stubClient({
+        get_frontend_views(id, cb) {
+          cb(null, views)
+        }
+      })
+    })
+    it('should return a promise that resolves to the views results', function(done) {
+      return mapdcon.getFrontendViewsAsync().then((result) => {
+        expect(result).to.deep.equal(views)
+        done()
+      })
+    })
+
+  })
+})
