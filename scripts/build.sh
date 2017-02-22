6183456412d625b2ecef0df1307d7eb0b7e52e01
ʈᵃᵢ <tdupreetan@gmail.com>
2017-02-22 13:38:30 -0800

    [infra] cleans up build script adds webpack --bail (#2302)
    * remove yarn cache clean
    
    * remove dead dc postinstall

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,3 @@
-npm run babel | tee build.log
-if [ "${PIPESTATUS[0]}" != "0" ]; then exit 1; fi
-
-npm run webpack | tee build.log
+npm run webpack --bail | tee build.log
 if [ "${PIPESTATUS[0]}" != "0" ]; then exit 1; fi
 
