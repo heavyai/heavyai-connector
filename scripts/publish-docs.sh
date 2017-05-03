3fbf3eab22d9268632d3b748f42152c53fa510b5
Marc Balaban <marc@marcbalaban.com>
2017-05-03 11:28:52 -0700

    [DOCS] gh-pages docs automatically update when merging to master - updated package.json scripts - created after-success script: "publish-docs"

---
+++ scripts/publish-docs.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+if [ "$TRAVIS_BRANCH" = "master" ]
+  then
+  REPO_PATH=git@github.com:mapd/mapd-connector.git
+  DOWNLOAD_PATH=build_docs
+  COMMIT_USER="mapd-bot"
+  COMMIT_EMAIL="machines@mapd.com"
+
+  git config --global user.name "${COMMIT_USER}"
+  git config --global user.email "${COMMIT_EMAIL}"
+  rm -rf ${DOWNLOAD_PATH}
+  git clone "${REPO_PATH}" ${DOWNLOAD_PATH}
+  
+  cd ${DOWNLOAD_PATH}
+    yarn
+    git fetch --all
+    git checkout gh-pages
+    git pull --rebase origin master
+    npm run clean:docs
+    npm run docs:build
+    git add docs -f
+    CHANGESET=$(git rev-parse --verify HEAD)
+    git commit -m "Automated documentation build for changeset ${CHANGESET}."
+    git push origin gh-pages
+  cd ..
+  rm -rf ${DOWNLOAD_PATH}
+fi
