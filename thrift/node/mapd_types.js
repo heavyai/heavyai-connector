46e3f33dd72dbc86d5c3397440692ad2ce9b4ede
Drew Ritter <arittr@gmail.com>
2019-04-01 20:59:11 -0700

    [FE-8254] bump connector for thrift (#139)
    * Update Thrift
    
    * adding in built dist

--- thrift/node/mapd_types.js
+++ thrift/node/mapd_types.js
@@ -75,6 +75,12 @@ ttypes.TImportHeaderRow = {
   'NO_HEADER' : 1,
   'HAS_HEADER' : 2
 };
+ttypes.TRole = {
+  'SERVER' : 0,
+  'AGGREGATOR' : 1,
+  'LEAF' : 2,
+  'STRING_DICTIONARY' : 3
+};
 ttypes.TMergeType = {
   'UNION' : 0,
   'REDUCE' : 1
@@ -1760,6 +1766,7 @@ var TCopyParams = module.exports.TCopyParams = function(args) {
   this.geo_coords_srid = 4326;
   this.sanitize_column_names = true;
   this.geo_layer_name = null;
+  this.s3_endpoint = null;
   if (args) {
     if (args.delimiter !== undefined && args.delimiter !== null) {
       this.delimiter = args.delimiter;
@@ -1823,6 +1830,9 @@ var TCopyParams = module.exports.TCopyParams = function(args) {
     }
     if (args.geo_layer_name !== undefined && args.geo_layer_name !== null) {
       this.geo_layer_name = args.geo_layer_name;
+    }
+    if (args.s3_endpoint !== undefined && args.s3_endpoint !== null) {
+      this.s3_endpoint = args.s3_endpoint;
     }
   }
 };
@@ -1987,6 +1997,13 @@ TCopyParams.prototype.read = function(input) {
         input.skip(ftype);
       }
       break;
+      case 22:
+      if (ftype == Thrift.Type.STRING) {
+        this.s3_endpoint = input.readString();
+      } else {
+        input.skip(ftype);
+      }
+      break;
       default:
         input.skip(ftype);
     }
@@ -2101,6 +2118,11 @@ TCopyParams.prototype.write = function(output) {
   if (this.geo_layer_name !== null && this.geo_layer_name !== undefined) {
     output.writeFieldBegin('geo_layer_name', Thrift.Type.STRING, 21);
     output.writeString(this.geo_layer_name);
+    output.writeFieldEnd();
+  }
+  if (this.s3_endpoint !== null && this.s3_endpoint !== undefined) {
+    output.writeFieldBegin('s3_endpoint', Thrift.Type.STRING, 22);
+    output.writeString(this.s3_endpoint);
     output.writeFieldEnd();
   }
   output.writeFieldStop();
@@ -2611,6 +2633,7 @@ var TServerStatus = module.exports.TServerStatus = function(args) {
   this.edition = null;
   this.host_name = null;
   this.poly_rendering_enabled = null;
+  this.role = null;
   if (args) {
     if (args.read_only !== undefined && args.read_only !== null) {
       this.read_only = args.read_only;
@@ -2632,6 +2655,9 @@ var TServerStatus = module.exports.TServerStatus = function(args) {
     }
     if (args.poly_rendering_enabled !== undefined && args.poly_rendering_enabled !== null) {
       this.poly_rendering_enabled = args.poly_rendering_enabled;
+    }
+    if (args.role !== undefined && args.role !== null) {
+      this.role = args.role;
     }
   }
 };
@@ -2698,6 +2724,13 @@ TServerStatus.prototype.read = function(input) {
         input.skip(ftype);
       }
       break;
+      case 8:
+      if (ftype == Thrift.Type.I32) {
+        this.role = input.readI32();
+      } else {
+        input.skip(ftype);
+      }
+      break;
       default:
         input.skip(ftype);
     }
@@ -2742,6 +2775,11 @@ TServerStatus.prototype.write = function(output) {
   if (this.poly_rendering_enabled !== null && this.poly_rendering_enabled !== undefined) {
     output.writeFieldBegin('poly_rendering_enabled', Thrift.Type.BOOL, 7);
     output.writeBool(this.poly_rendering_enabled);
+    output.writeFieldEnd();
+  }
+  if (this.role !== null && this.role !== undefined) {
+    output.writeFieldBegin('role', Thrift.Type.I32, 8);
+    output.writeI32(this.role);
     output.writeFieldEnd();
   }
   output.writeFieldStop();
