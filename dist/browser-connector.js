986e42fa5cbbbc682b9255130b6cc22824721067
Matthew Marcus <lurchbulldog@gmail.com>
2019-04-11 17:26:02 -0500

    Allows for the rest of the dataSource descriptor fields to be passed through

--- dist/browser-connector.js
+++ dist/browser-connector.js
@@ -144,6 +144,8 @@
 
 	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -1321,12 +1323,15 @@
 	      var _this6 = this;
 
 	      this._client[0].get_table_details(this._sessionId[0], tableName, function (error, fields) {
+	        console.log("fields => ", fields);
 	        if (fields) {
 	          var rowDict = fields.row_desc.reduce(function (accum, value) {
 	            accum[value.col_name] = value;
 	            return accum;
 	          }, {});
-	          callback(null, _this6.convertFromThriftTypes(rowDict));
+	          callback(null, _extends({}, fields, {
+	            columns: _this6.convertFromThriftTypes(rowDict)
+	          }));
 	        } else {
 	          callback(new Error("Table (" + tableName + ") not found" + error));
 	        }
