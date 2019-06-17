3c480c05b537509d3c9cceb725236dea40453b7e
Jonathan Rajavuori <jrajav@gmail.com>
2019-06-17 11:11:17 -0500

    Update Node example for API response format changes

--- examples/node.js
+++ examples/node.js
@@ -1,29 +1,29 @@
 /* eslint-disable */
 
-const Connector = require("../dist/node-connector.js")
+const Connector = require('../dist/node-connector.js')
 
 // The total number of tweets from Columbia
-const query = "SELECT count(*) AS n FROM tweets_nov_feb WHERE country='CO'"
+const query = 'SELECT count(*) AS n FROM tweets_nov_feb WHERE country=\'CO\''
 const query2 =
-  "SELECT carrier_name as key0, AVG(airtime) AS val FROM flights_donotmodify WHERE airtime IS NOT NULL GROUP BY key0 ORDER BY val DESC LIMIT 100"
+    'SELECT carrier_name as key0, AVG(airtime) AS val FROM flights_donotmodify WHERE airtime IS NOT NULL GROUP BY key0 ORDER BY val DESC LIMIT 100'
 const defaultQueryOptions = {}
 
 const connector = new Connector()
 
 
 connector
-  .protocol("https")
-  .host("metis.mapd.com")
-  .port("443")
-  .dbName("mapd")
-  .user("mapd")
-  .password("HyperInteractive")
+  .protocol('https')
+  .host('metis.mapd.com')
+  .port('443')
+  .dbName('mapd')
+  .user('mapd')
+  .password('HyperInteractive')
   .connectAsync()
   .then(session =>
     // now that we have a session open we can make some db calls:
     Promise.all([
       session.getTablesAsync(),
-      session.getFieldsAsync("flights_donotmodify"),
+      session.getFieldsAsync('flights_donotmodify'),
       session.queryAsync(query, defaultQueryOptions),
       session.queryAsync(query2, defaultQueryOptions)
     ])
@@ -32,25 +32,24 @@ connector
   .then(values => {
     // handle result of getTablesAsync
     console.log(
-      "All tables available at metis.mapd.com:\n\n",
+      'All tables available at metis.mapd.com:\n\n',
       values[0].map(x => x.name)
     )
 
-    // handle result of getFieldsAsync
-    console.log(
-      "\nAll fields for 'flights_donotmodify':\n\n",
-      values[1].reduce((o, x) => Object.assign(o, { [x.name]: x }), {})
-    )
+// handle result of getFieldsAsync
+console.log(
+    '\nAll fields for \'flights_donotmodify\':\n\n',
+    values[1].columns.reduce((o, x) => Object.assign(o, {[x.name]: x}), {}))
 
-    // handle result of first query
-    console.log("\nQuery 1 results:\n\n", Number(values[2][0].n))
+// handle result of first query
+console.log('\nQuery 1 results:\n\n', Number(values[2][0].n))
 
-    // handle result of second query
+// handle result of second query
     console.log(
-      "\nQuery 2 results:\n\n",
+      '\nQuery 2 results:\n\n',
       values[3].reduce((o, x) => Object.assign(o, { [x.key0]: x.val }), {})
     )
   })
   .catch(error => {
-    console.error("Something bad happened: ", error)
+    console.error('Something bad happened: ', error)
   })
