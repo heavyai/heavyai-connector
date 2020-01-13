c92e1e22c263178f408ecde184751aad1258ac0c
Tamara Chu <tamara.chu@omnisci.com>
2020-01-13 11:58:59 -0800

    Remove unused createResultError

--- src/wrap-with-error-handling.js
+++ src/wrap-with-error-handling.js
@@ -1,24 +1,11 @@
 const MapDClient =
   (typeof window !== "undefined" && window.MapDClient) ||
   require("../build/thrift/node/mapd.thrift.js").Client // eslint-disable-line global-require
-const TMapDException =
-  (typeof window !== "undefined" && window.TMapDException) ||
-  require("../build/thrift/node/mapd_types.js").TMapDException // eslint-disable-line global-require
 const Thrift =
   (typeof window !== "undefined" && window.Thrift) || require("thrift").Thrift // eslint-disable-line global-require
 
 export function isResultError(result) {
   return result instanceof Thrift.TException || result instanceof Error
-}
-
-export function createResultError(result) {
-  if (result instanceof TMapDException) {
-    return new Error(result.error_msg)
-  } else if (typeof result.message === "undefined") {
-    return new Error("Unspecified Error")
-  } else {
-    return new Error(result.message)
-  }
 }
 
 /* eslint-disable consistent-this */
