6e84cb51ebfc3c2c6ca368192019ee8bb67dc03a
Jonathan Rajavuori <jrajav@gmail.com>
2020-03-10 15:23:38 -0500

    Add noop module and resolve Node requires to it in browser webpack build

---
+++ src/noop.js
@@ -0,0 +1,6 @@
+// This module exists only to stand in for Node imports for the browser
+// connector, for the unused require()'s of Node modules guarded by
+// isNodeRuntime() in map-con-es6, until we have a better solution for
+// conditional imports.
+
+module.exports = {}
