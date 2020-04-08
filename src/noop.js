60a1e5837916ea8fc5a525cae44f65149f85577a
Jonathan Rajavuori <jrajav@gmail.com>
2020-04-08 09:09:10 -0500

    Remove noop externals

--- src/noop.js
+++
@@ -1,6 +0,0 @@
-// This module exists only to stand in for Node imports for the browser
-// connector, for the unused require()'s of Node modules guarded by
-// isNodeRuntime() in map-con-es6, until we have a better solution for
-// conditional imports.
-
-module.exports = {}
