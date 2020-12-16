9768a4623b95152f17ed683a8f55a873de982b2a
Joel Clay <joel.clay@omnisci.com>
2020-12-16 18:19:21 -0500

    Revert Arrow import change

--- src/mapd-con-es6.js
+++ src/mapd-con-es6.js
@@ -23,7 +23,7 @@ import EventEmitter from "eventemitter3"
 
 import MapDClientV2 from "./mapd-client-v2"
 import processQueryResults from "./process-query-results"
-import * as Arrow from "apache-arrow"
+import { Table } from "apache-arrow"
 
 const COMPRESSION_LEVEL_DEFAULT = 3
 
@@ -1106,7 +1106,7 @@ export class MapdCon {
       TArrowTransport.WIRE,
       (err, data) => {
         const buf = Buffer.from(data.df_buffer, "base64")
-        const arrowTable = Arrow.Table.from(buf)
+        const arrowTable = Table.from(buf)
         return callback(err, arrowTable)
       }
     ]
