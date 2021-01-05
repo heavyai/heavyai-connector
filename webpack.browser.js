2d4159fd9f1f0fab13ad3960643b213e0a0df301
Joel Clay <joel.clay@omnisci.com>
2021-01-05 17:14:43 -0500

    Fix to properly export module as Webpack expects
    Docs recommend not using an array for the entry point, so the includes
    have been moved to the entrypoint.
    
    Use the correct ES6 export syntax to export the module

--- webpack.browser.js
+++ webpack.browser.js
@@ -2,16 +2,7 @@ const path = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  entry: [
-    "script-loader!./build/thrift/browser/thrift.js",
-    "script-loader!./build/thrift/browser/common_types.js",
-    "script-loader!./build/thrift/browser/serialized_result_set_types.js",
-    "script-loader!./build/thrift/browser/omnisci_types.js",
-    "script-loader!./build/thrift/browser/OmniSci.js",
-    "script-loader!./build/thrift/browser/completion_hints_types.js",
-    "./src/mapd-con-es6.js",
-    "./src/entry.browser.js"
-  ],
+  entry: "./src/entry.browser.js",
   plugins: [
     new webpack.ProgressPlugin(),
     new webpack.ProvidePlugin({
@@ -33,7 +24,7 @@ module.exports = {
     ]
   },
   externals: {
-    'apache-arrow': 'umd Arrow'
+    '@apache-arrow/es2015-umd': 'umd Arrow'
   },
   resolve: {
     fallback: {
